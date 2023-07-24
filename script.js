87b233fc629cf9a5861b3a398da86fe7ba4aa033
unknown <gracjan.zalewski55@gmail.com>
2023-07-24 22:12:08 +0200

    FIX COMMA BUGS

--- script.js
+++ script.js
@@ -1,30 +1,55 @@
 const buttons = document.querySelectorAll(".numbers");
 const operators = document.querySelectorAll(".operators");
-const result = document.querySelector("#result");
+const resultPlace = document.querySelector("#result");
 const clear = document.querySelector("#clear");
 const back = document.querySelector("#back");
-let operation = [""];
+const submit = document.querySelector("#submit");
+const allButtons = document.querySelectorAll(".buttons");
+const wholeOperation = document.querySelector("#whole-operation");
+const comma = document.querySelector("#comma");
+let result;
+let operation = [];
 let operator = "";
 let i = 0;
+let operationKey;
+resultPlace.innerHTML = 0;
 buttons.forEach((firstChose) => {
   firstChose.addEventListener("click", function () {
-    operation += firstChose.innerHTML;
+    operation.push(firstChose.innerHTML);
     i = 0;
-    alert(operation);
-    
+    let showedNumber = firstChose.innerHTML;
+    resultPlace.innerHTML = showedNumber;
   });
 });
+
 operators.forEach((operatorChose) => {
   operatorChose.addEventListener("click", function () {
     i++;
+
     if (i <= 1) {
-      operation += operatorChose.innerHTML;
+      operation.push(operatorChose.innerHTML);
+      operationKey = operatorChose.innerHTML;
     }
+    console.log(operation[1]);
   });
 });
-clear.addEventListener("click", function(){
-    operation = [""]
+allButtons.forEach((button) => {
+  button.addEventListener("click", function () {
+    wholeOperation.innerHTML = operation.join("");
+  });
 });
-back.addEventListener("click", function(){
-    operation = operation.slice(0,-1);
-})
+clear.addEventListener("click", function () {
+  operation = [""];
+  resultPlace.innerHTML = 0;
+});
+back.addEventListener("click", function () {
+  operation = operation.slice(0, -1);
+});
+submit.addEventListener("click", function () {
+  result = eval(operation.join(""));
+  if (result === Infinity) {
+    resultPlace.innerHTML = "Nie można dzielić przez 0";
+  } else {
+    resultPlace.innerHTML = result;
+  }
+});
