6db30f625163b6649288ca334293bfd68d719509
unknown <gracjan.zalewski55@gmail.com>
2023-07-25 22:24:41 +0200

    ADD DESIGN AND FIX BUGS

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+/* BUGI
+
+*/
+/* TODO 
+
+-Dodanie oprawy graficznej
+-Rozwinięcie liczb po przecinku
+-Zaokrąglenie liczby za pomocą round
+*/
 const buttons = document.querySelectorAll(".numbers");
 const operators = document.querySelectorAll(".operators");
 const resultPlace = document.querySelector("#result");
@@ -6,15 +15,28 @@ const back = document.querySelector("#back");
 const submit = document.querySelector("#submit");
 const allButtons = document.querySelectorAll(".buttons");
 const wholeOperation = document.querySelector("#whole-operation");
-const comma = document.querySelector("#comma");
+const percent = document.querySelector("#percent");
+const history = document.querySelector("#operations-history");
+const clearHistory = document.querySelector("#clear-history");
+let isSubmited = false;
+let percentOperation = 0;
+let isPercentClicked = false;
 let result;
 let operation = [];
+
+let targetedOperation = "";
 let operator = "";
 let i = 0;
 let operationKey;
 resultPlace.innerHTML = 0;
+
 buttons.forEach((firstChose) => {
   firstChose.addEventListener("click", function () {
+    if(isSubmited === true){
+      resultPlace.innerHTML = 0;
+      operation = [];
+      isSubmited = false;
+    }
     operation.push(firstChose.innerHTML);
     i = 0;
     let showedNumber = firstChose.innerHTML;
@@ -22,6 +44,10 @@ buttons.forEach((firstChose) => {
   });
 });
 
+percent.addEventListener("click", function () {
+  isPercentClicked = true;
+  percentOperation = operation[0] / 100;
+});
 operators.forEach((operatorChose) => {
   operatorChose.addEventListener("click", function () {
     i++;
@@ -30,7 +56,6 @@ operators.forEach((operatorChose) => {
       operation.push(operatorChose.innerHTML);
       operationKey = operatorChose.innerHTML;
     }
-    console.log(operation[1]);
   });
 });
 allButtons.forEach((button) => {
@@ -40,16 +65,30 @@ allButtons.forEach((button) => {
 });
 clear.addEventListener("click", function () {
   operation = [""];
+  wholeOperation.innerHTML = "";
   resultPlace.innerHTML = 0;
 });
 back.addEventListener("click", function () {
   operation = operation.slice(0, -1);
+  wholeOperation.innerHTML = wholeOperation.innerHTML.slice(0, -1);
+  i = 0;
 });
 submit.addEventListener("click", function () {
+  isSubmited = true;
   result = eval(operation.join(""));
   if (result === Infinity) {
     resultPlace.innerHTML = "Nie można dzielić przez 0";
+  }
+  if (isPercentClicked === true) {
+    resultPlace.innerHTML = percentOperation;
   } else {
     resultPlace.innerHTML = result;
   }
+  targetedOperation = operation.join("");
+
+  history.innerHTML += targetedOperation + "=" + result;
+  history.innerHTML += "<br>";
+});
+clearHistory.addEventListener("click", function () {
+  history.innerHTML = "Historia" + "<br>";
 });
